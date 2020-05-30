cdbb56e5d2000233f5c3e93e8f5434987af825ab
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:12:14 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild("midiv")
+  divRef: ElementRef;
+
+  @ViewChild(TimerComponent)
+  componenteRef: TimerComponent;
+
   title = 'elementRefViewchild';
+
+  ngAfterViewInit(): void {
+    console.log(this.divRef);  
+    this.divRef.nativeElement.innerHTML = "Hola Curso Element Ref"; 
+  }
+
+  eventoAction1() {
+    console.log('Llego aqui' + this.componenteRef);
+    this.componenteRef.incrementar();
+  }
+
+  eventoAction2() {
+    console.log('Llego aqui' + this.componenteRef);
+    this.componenteRef.decrementar();
+  }
+
 }
